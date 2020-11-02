ad42b03cf4b3e5e75e92fd6fa0961b7d2627e8a8
Matheus Bastos <matheus.mab432@gmail.com>
2020-11-02 15:38:00 -0300

    Adicionando alguns algoritmos de pesquisa

---
+++ AlgoritmosPesquisa/BinarySearch.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace AlgoritmosPesquisa
+{
+    public static class BinarySearch
+    {
+        /// <summary>
+        /// Observação; Binary Search é um algoritmo de pesquisa que é funcional apenas se a coleção atual
+        /// já estiver ordenada, logo, o algoritmo abaixo assume que a coleção já esteja ordenada em ordem
+        /// crescente para que a sua lógica seja funcional.
+        /// </summary>
+        /// <param name="arr">Array ordenado que será percorrido até achar num</param>
+        /// <param name="num">Valor a ser buscado</param>
+        /// <param name="inicio">Início do subarray atual, valor padrão é 0 para o array original</param>
+        /// <param name="fim">Fim do subarray atual, valor padrão é null e será convertido para arr.Length para o array original</param>
+        /// <returns></returns>
+        public static bool Search(int[] arr, int num, int inicio = 0, int? fim = null)
+        {
+            if (fim is null)
+            {
+                fim = arr.Length;
+            }
+
+            if (fim - inicio < 2)
+            {
+                return arr[inicio] == num;
+            }
+            int meio = (int)((inicio + fim) / 2);
+            Console.WriteLine($"arr[meio] = {arr[meio]} em {num}");
+            if (num == arr[meio])
+            {
+                return true;
+            }
+            else if (num < arr[meio])
+            {
+                return Search(arr, num, inicio, meio - 1);
+            }
+            else
+            {
+                return Search(arr, num, meio + 1, fim);
+            }
+        }
+
+        /* --------------------------Versão explicada-----------------------------
+        public static bool Search(int[] arr, int num, int inicio=0, int? fim=null)
+        {
+            if (fim is null)
+            {
+                fim = arr.Length;
+            }
+
+            if (fim - inicio < 2)
+            {
+                return arr[inicio] == num;
+            }
+            // Todo elemento à esquerda do índice meio será < arr[meio] e todo elemento à direita será > arr[meio]
+            int meio = (int)((inicio + fim) / 2);
+            Console.WriteLine($"arr[meio] = {arr[meio]} em {num}");
+            // Se num for igual ao valor no meio do array atual, retornar true pois o valor está presente na coleção
+            if (num == arr[meio])
+            {
+                return true;
+            }
+            // Se num for menor que o valor no meio do array, buscar no subarray da esquerda
+            else if (num < arr[meio])
+            {
+                return Search(arr, num, inicio, meio - 1);
+            }
+            // Se num for maior que o valor no meio do array, buscar no subarray da direita
+            else
+            {
+                return Search(arr, num, meio + 1, fim);
+            }
+        }
+        */
+    }
+}
---
+++ AlgoritmosPesquisa/BinarySearchTree.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+
+namespace AlgoritmosPesquisa
+{
+    public static class BinarySearchTree
+    {
+        public static Node NewNode(int value)
+        {
+            var root = new Node();
+            root.value = value;
+            root.left = null;
+            root.right = null;
+            return root;
+        }
+
+        public static Node InsertNode(Node root, int value)
+        {
+            if (root is null)
+            {
+                root = NewNode(value);
+            }
+            else if (value < root.value)
+            {
+                root.left = InsertNode(root.left, value);
+            }
+            else
+            {
+                root.right = InsertNode(root.right, value);
+            }
+            return root;
+        }
+
+        public static Node InsertNode(Node root, params int[] values)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                root = InsertNode(root, values[i]);
+            }
+            return root;
+        }
+
+        public static void InOrder(Node root)
+        {
+            if (root is null)
+                return;
+            InOrder(root.left);
+            Console.Write($"{root.value}  ");
+            InOrder(root.right);
+        }
+
+        public static bool Search(Node tree, int value)
+        {
+            if (tree is null)
+            {
+                return false;
+            }
+            else if (value == tree.value)
+            {
+                return true;
+            }
+            else if (value < tree.value)
+            {
+                return Search(tree.left, value);
+            }
+            else
+            {
+                return Search(tree.right, value);
+            }
+        }
+
+        public static Dictionary<int, bool> Search(Node tree, params int[] values)
+        {
+            Dictionary<int, bool> numbersInTree = new Dictionary<int, bool>();
+            for (int i = 0; i < values.Length; i++)
+            {
+                numbersInTree.Add(values[i], Search(tree, values[i]));
+            }
+            return numbersInTree;
+        }
+    }
+
+    public class Node
+    {
+        public int value { get; set; }
+        public Node left { get; set; }
+        public Node right { get; set; }
+    }
+}
